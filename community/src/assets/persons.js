916f67e776f46888eae96c03ee3c88c16c45c51c
Rasim <56521004+rasimab@users.noreply.github.com>
2021-01-02 19:31:01 +0400

    Update persons.js

--- community/src/assets/persons.js
+++ community/src/assets/persons.js
@@ -47,6 +47,23 @@ export const data = {
         city: "Baku",
         country: "Azerbaijan",
       },
+    },{
+      id: uuidv4(),
+      name: "Rasim Abdullayev",
+      img: "hhttps://avatars2.githubusercontent.com/u/56521004?v=4",
+      links: {
+        website: "https://goupaz.com",
+        linkedin: "https://www.linkedin.com/in/abdullayevrs/",
+        github: "https://github.com/rasimab",
+      },
+      jobTitle: "Community Leader",
+      location: {
+        city: "Baku",
+        country: "Azerbaijan",
+      },
     },
+    
+    
+    
   ],
 }
